0fa935fe235b164e8e6523f04d0ea84c18ce27e2
18arihant <arihant18082003@gmail.com>
2023-04-04 21:52:31 +0530

    Types of Operators

--- tut7.cpp
+++ tut7.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-    //Aritmetic operators
+    //Types oof operators:
+    //1. Aritmetic operators
     int a=4, b=5;
     cout<<"The valure of a+b is"<<a+b<<endl;
     cout<<"The valure of a-b is"<<a-b<<endl;
@@ -15,7 +16,7 @@ int main()
     cout<<"The valure of ++a is"<<++a<<endl;//first it will increement the value of a by 1 then it will print a=5
     cout<<"The valure of --a is"<<--a<<endl;//first it will decreement the value of a by 1 then it will print a=4
     cout<<endl;
-    // comparison operators
+    // 2. comparison operators
     cout<<"The value of a==b is"<<(a==b)<<endl;
     cout<<"The value of a!=b is"<<(a!=b)<<endl;
     cout<<"The value of a>=b is"<<(a>=b)<<endl;
@@ -23,7 +24,7 @@ int main()
     cout<<"The value of a>b is"<<(a>b)<<endl;
     cout<<"The value of a<b is"<<(a<b)<<endl;
     cout<<endl;
-    //logical operators
+    //3. logical operators (it only gives 0 or 1 i.e. for true --> 1 and for false -->0 )
     cout<<"The value of this logical operation is"<<((a==b) && (a<b))<<endl;
     cout<<"The value of this logical operation is"<<(!(a==b))<<endl;
     cout<<"The value of this logical operation is"<<((a==b) || (a<b))<<endl;
