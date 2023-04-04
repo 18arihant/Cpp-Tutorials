61b8dc323d526c405fee9e989ed30ec7c9569a6c
18arihant <arihant18082003@gmail.com>
2023-04-04 21:44:48 +0530

    sum of two numbers by taking input

--- tut5.cpp
+++ tut5.cpp
@@ -8,7 +8,8 @@ int main()
     cin>>a; //">>" this is extraction operator
     cout<<"Enter the value of num2:\n";
     cin>>b; //">>" this is extraction operator
-    int c=a+b;
+    int c;
+    c=a+b;
     cout<<"The sum off two number:"<<a <<"and"<< b<<"is-"<<c<<endl;
     return 0;
 }
