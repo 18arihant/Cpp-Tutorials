40fe9431ce6460791cbf1ba0133242f7ad570ef8
18arihant <arihant18082003@gmail.com>
2023-04-04 21:32:46 +0530

    Types of Comments

--- tut2.cpp
+++ tut2.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
+// comments are of 2 types:
+// 1. single line cooment
 //This program is created by ARIHANT.
-/* abjabhsshsdb*/
+//2. multiline comments
+/* hello i am 
+Arihant Aggarwal*/
+
 int main()
 {
     cout<<"Hello World"<<endl;
