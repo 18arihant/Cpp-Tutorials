d8e159beb3e016aac451957ed2077cbdf1ce1916
18arihant <arihant18082003@gmail.com>
2023-04-04 21:48:36 +0530

    Types of header files

--- tut6.cpp
+++ tut6.cpp
@@ -1,7 +1,7 @@
-// there are two types of header files:
+// there are 2 types of header files:
 // 1. system header files: it comes with the compiler
 #include<iostream>
-// 2. user defi ned header files: it is written by the programmer
+// 2. user defined header files: it is written by the programmer
 // #include"this.h" --> this will produce error if this.h is not present in the current directory.
 
 using namespace std;
