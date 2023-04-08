c2ee189681760dacf51f33e51937e5a80b0b5338
18arihant <arihant18082003@gmail.com>
2023-04-08 22:43:23 +0530

    first program

--- tut1.cpp
+++ tut1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
-   cout<<"Hello world, this is my first c++ program"<<endl; //"<<" this operator is known as insertion operator
+   cout<<"Hello world, this is my first program in c++"<<endl; 
+   //"<<" this operator is known as insertion operator
     return 0;
 }
