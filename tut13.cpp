586d511e821aa5ce3cee4a1e406e1110a451b49e
18arihant <arihant18082003@gmail.com>
2023-04-09 12:24:38 +0530

    Pointers in c++

--- tut13.cpp
+++ tut13.cpp
@@ -10,9 +10,9 @@ int main()
     int *b =&a;
      //&--- (address) of pointer
     cout<<"The address of a is: "<<b<<endl;// b has the address of a stored in memory
-    cout<<"The address of a is: "<<&a<<endl;// we can use b or &a
-    
-    //*--- dereference oper ator
+    cout<<"The address of a is: "<<&a<<endl;// we can use b or &a \
+        
+    //*--- dereference operator
     cout<<"The value at address b is "<<*b<<endl;
 
     return 0;
