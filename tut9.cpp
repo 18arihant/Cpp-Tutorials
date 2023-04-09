2150ce5d3fdd5d701d48adcf0a92d831b9465c88
18arihant <arihant18082003@gmail.com>
2023-04-09 12:06:25 +0530

    constant variables

--- tut9.cpp
+++ tut9.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 
+// declaring contants
 int main()
 {
     const int  a=45; // the value of a will be 45 only and it cannot be changed 
     cout<<a<<endl;
-    a=6; // error will come as a is constant
+    // a=6; // error will come as a is constant
     cout<<a<<endl;
     return 0;
 }
