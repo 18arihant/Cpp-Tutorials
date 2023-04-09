14e18ad1997e84f9d31a76356b99fffb7ed3ba55
18arihant <arihant18082003@gmail.com>
2023-04-09 12:20:54 +0530

    while loop

--- tut12.2.cpp
+++ tut12.2.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 
+//while loop
 int main()
 {
     int i=0;
-    while(i<=10){
+    while(i<=10)
+    {
         cout<<i<<endl;
         i++;
-        
     }
     return 0;
 
-}
+}   
