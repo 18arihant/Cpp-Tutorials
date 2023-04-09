706e5a83a7b73d94a819ba6f8b1d38f155f19122
18arihant <arihant18082003@gmail.com>
2023-04-09 11:56:02 +0530

    global and local variables

--- tut4.cpp
+++ tut4.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
+
 //local and global variable can have same names
-int glo=9; // global variable
+int glo=9; //  declaring global variable
 void sum()
 {
-    cout<<glo<<endl; // it will print the value og gllobal variablr
+    cout<<glo<<endl; // it will print the value of global variable
 }
 int main()
 {
-    int glo=1; //local variable, local variable takes precedence over global
+    int glo=1; //local variable, local variable takes precedence over global variable
     glo=78; // value of local variable gets updated
     cout<<glo<<endl; // value of updated local will be printed
     sum(); // value of global will be printed
